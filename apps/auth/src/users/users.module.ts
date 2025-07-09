b82d40b4e0cf190a487986f6c10d63681f0eca24
Krzysztof <krzysztof.dydak@it-solve.pl>
2025-07-09 21:22:04 +0200

    feat: auth microservice - users modules and post request

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { UserDocument, UserSchema } from './models/users.model';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+    LoggerModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
+export class UsersModule {}
